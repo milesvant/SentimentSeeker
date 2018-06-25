cd62c10cca91d23e57baccb32af747d48867de0e
Miles Van Tongeren <mnv2108@columbia.edu>
2018-06-24 18:51:01 -0700

    More environment var refactoring

--- config.py
+++ config.py
@@ -28,7 +28,4 @@ class Config(object):
     LANGUAGES = ['en', 'es']
     CELERY_BROKER_URL = 'redis://localhost:6379/0'
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-    REDIS_HOST = 'localhost'
-    REDIS_PASSWORD = ''
-    REDIS_PORT = 6379
-    REDIS_URL = 'redis://localhost:6379/0'
+    REDIS_URL = os.environ.get('REDIS_URL')
